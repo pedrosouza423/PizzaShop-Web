6f951fffc9842a3d9f3e8616c00e239960c35db1
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-10 16:22:17 -0300

    Application Layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
+
 import { router } from './routes'
 
 export function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />
 }
