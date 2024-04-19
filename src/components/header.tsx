0b6762e82a5dbab5d1e155c624af3803cbe7f7ce
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-19 12:16:06 -0300

    Added ThemeToggle and account menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
-import { Link } from 'react-router-dom'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export const Header = () => {
@@ -21,6 +22,11 @@ export const Header = () => {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
