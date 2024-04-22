a59eb9529aeae8663ac32d875451ed271208c184
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-22 18:36:05 -0300

    Added Revenue graph for the period

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { RevenueChat } from './revenue-chat'
 
 export const Dashboard = () => {
   return (
@@ -16,6 +17,9 @@ export const Dashboard = () => {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChat />
         </div>
       </div>
     </>
---
+++ src/pages/app/dashboard/revenue-chat.tsx
@@ -0,0 +1,69 @@
+import { X } from 'lucide-react'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '01/01', revenue: 1200 },
+  { date: '02/01', revenue: 200 },
+  { date: '03/01', revenue: 800 },
+  { date: '04/01', revenue: 900 },
+  { date: '05/01', revenue: 1100 },
+  { date: '06/01', revenue: 700 },
+  { date: '07/01', revenue: 2200 },
+]
+
+export const RevenueChat = () => {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pr-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line
+              type="linear"
+              stroke={colors.violet[500]}
+              dataKey="revenue"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
