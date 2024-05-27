0a63761be93e1214c8bfd44327e88fe95c06cd09
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-27 16:31:56 -0300

    Added metrics loading

--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -4,13 +4,14 @@ import { DollarSign } from 'lucide-react'
 import { getMonthRevenue } from '@/api/get-month-revenue'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
 export const MonthRevenueCard = () => {
   const { data: monthRevenue } = useQuery({
     queryKey: ['metrics', 'month-revenue'],
     queryFn: getMonthRevenue,
   })
 
-  console.log(monthRevenue)
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -20,7 +21,7 @@ export const MonthRevenueCard = () => {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthRevenue && (
+        {monthRevenue ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {(monthRevenue.receipt / 100).toLocaleString('pt-BR', {
@@ -46,6 +47,8 @@ export const MonthRevenueCard = () => {
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
