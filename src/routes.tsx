225868e6e9bd90d38126cb4e3f1c627bd63ea3fd
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-28 12:23:31 -0300

    Errors and 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -27,4 +28,6 @@ export const router = createBrowserRouter([
       { path: 'sign-up', element: <SignUp /> },
     ],
   },
+
+  { path: '*', element: <NotFound /> },
 ])
